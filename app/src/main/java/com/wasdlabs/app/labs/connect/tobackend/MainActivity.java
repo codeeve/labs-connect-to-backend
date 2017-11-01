e3c675d477a1d0e0ab8fc97704d1bf049ed6f57a
Kasmir <rpgmania37@gmail.com>
2017-11-01 10:09:56 +0800

    first commit

---
+++ app/src/main/java/com/wasdlabs/app/labs/connect/tobackend/MainActivity.java
@@ -0,0 +1,13 @@
+package com.wasdlabs.app.labs.connect.tobackend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
